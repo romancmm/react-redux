940baa84578f61c764015f058438447dc47fd61d
romancmm <romancmm@gmail.com>
2023-12-26 03:43:46 +0600

    set-up redux toolkit in the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+ plugins: [react()],
+ resolve: {
+  // alias: {
+  //  '@/': path.resolve(__dirname, './src'),
+  //  '@/components': path.resolve(__dirname, './src/components'),
+  //  '@/redux': path.resolve(__dirname, './src/redux'),
+  //  '@/lib': path.resolve(__dirname, './src/lib'),
+  //  '@/utils': path.resolve(__dirname, './src/utils'),
+  //  '@/pages': path.resolve(__dirname, './src/pages'),
+  // },
+ },
 })
